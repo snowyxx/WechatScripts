07ed4202ae85426934d272314bf550a9832d70df
snowyxx <snowyxx@126.com>
2016-09-30 14:47:58 +0800

    bat update

--- WeChatJava/sendMsg.bat
+++ WeChatJava/sendMsg.bat
@@ -1,4 +1,9 @@
 @echo off
 rem java -cp .;lib/* Messager <@all> <Your [orther messga]>
 rem java -cp .;lib/* Messager <tagNames> <alarmurl> <severity> <alertType> <alertDate> <device> <monitorGroup> <Your [orther messga]>
+rem example: 
+rem sendMsg.bat ME测试 www.bing.com "$SEVERITY" "$MONITORTYPE" "$DATE" "$MONITORNAME" "$ATTRIBUTE" $RCAMSG_PLAINTEXT
+rem sendMsg.bat ME测试 www.bing.com 停止 应用服务器 1999-1-1 server1 CRM系统 CPU利用率过高 88%
+rem sendMsg.bat @all CPU利用率过高 88%
+
 "..\jre\bin\java" -cp .;lib/* Messager %*
